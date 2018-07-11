5c4b485836897bedf6a5445007ec1c16d10d09bd
gadabout-geek <dipendrabana@gmail.com>
2018-07-11 17:55:37 +0530

    codes added

--- codechef/testcase.cpp
+++ codechef/testcase.cpp
@@ -1,23 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	srand(time(0));
 	int t;
-	t = rand()%10;
+	//srand(time(0));
+	// cin>>t;
+	t = 1+rand()%10+4;
 	cout<<t<<endl;
 	while(t--){
-	int n = (rand()%30)+1;
-	cout<<n<<endl;
-	int a[n+1];
-	int b[n+2];
-	for(int i = 0; i < n;i++){
-		a[i] = (rand()%105)+5;
-		b[i] = rand()%(a[i])+1;
-		cout<<a[i]<<"\t";
+		int n = 1+rand()%10+3;
+		int k = 1+rand()%(n-3);
+		cout<<n<<" "<<k<<endl;
+		while(n--){
+			cout<<1+rand()%9985+10<<" ";
+		}
+		cout<<endl;
 	}
-	cout<<endl;
-	for(int i=0;i<n;i++){
-		cout<<b[i]<<"\t";
-	}
-	cout<<endl;
-}
 }
